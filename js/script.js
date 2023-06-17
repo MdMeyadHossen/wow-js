93dbb710919751fc02ffd2de3538692bbe426ab5
Md Meyad Hossen <122221406+MdMeyadHossen@users.noreply.github.com>
2023-06-18 00:12:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+wow = new WOW({
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  })
+wow.init();
+
+$(document).ready(function(){
+
+
+});
+
+
+
+
+
